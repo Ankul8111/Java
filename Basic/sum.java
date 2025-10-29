4a4593191a700cde2ada1c83ee0eb19d1b432a19
Ankul Kumar Rai <ankulkumarrai@gmail.com>
2025-10-29 22:23:18 +0530

    Upload

---
+++ Basic/sum.java
@@ -0,0 +1,9 @@
+package Basic;
+
+public class sum {
+    public static void main(String[] args) {
+        System.out.println(9 + 9);
+        System.out.println(9 + 2);
+        System.out.println(2 + 3);
+    }
+}
