eb295c10c2405a09c79b7adab40beaf7825ed629
Ankul Kumar Rai <ankulkumarrai@gmail.com>
2025-10-29 22:07:33 +0530

    Upload

---
+++ Basic/first.java
@@ -0,0 +1,10 @@
+package Basic;
+
+public class first {
+    public static void main(String[] args) {
+        System.out.println("Ankul Kumar Rai");
+        System.out.println(7 + 9);
+        System.out.println(9 * 9);
+        System.out.println(9 * 7 + 3);
+    }
+}
