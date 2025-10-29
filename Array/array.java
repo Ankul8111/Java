b8b6d72a00e53703e0b319e3c9b1447499c615c9
Ankul Kumar Rai <ankulkumarrai@gmail.com>
2025-10-29 22:29:23 +0530

    Upload

---
+++ Array/array.java
@@ -0,0 +1,11 @@
+package Array;
+
+public class array {
+    public static void main(String[] args) {
+        String[] num = {"Ankul", "Amit", "Ballu", "Nikhil", "Nitesh", "Neha"};
+
+        for(int i = 0; i < 6; i++) {
+            System.out.println(num[i]);
+        }
+    }
+}
